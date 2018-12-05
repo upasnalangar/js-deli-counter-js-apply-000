8c8bbc69c23543b714a3ba8614ac939f994c6e21
Upasna Langar <langarupasna.2@gmail.com>
2018-12-05 21:50:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -37,12 +37,20 @@ function currentLine(katzDeliLine)
       //Remove last letter from line
       line = line + laststring;
     return "The line is currently: " + line 
+    return "The line is currently: " + line ;
+=======
+>>>>>>> 53165700dd6d69b1e5113b5af6c2491cda064288
       for(var i=0;i<katzDeliLine.length; i++)
       {
       line = katzDeliLine[i]+1 + "." + katzDeliLine  // line =  1. person[0] + 2. person[1] +.person[n-1]
         
       }
     return "The line is currently: " + katzDeliLine[i]+1 + "."+ katzDeliLine;
+<<<<<<< HEAD
+=======
+>>>>>>> e26619fcbd1134d95adaaf85e893ed1283602244
+      
+>>>>>>> 53165700dd6d69b1e5113b5af6c2491cda064288
   }
   
   else
